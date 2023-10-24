6f6b9cfe6b090e6701543879e0c63eee8024f91d
bibinvninan32 <105633202+bibinvninan32@users.noreply.github.com>
2023-10-24 15:33:06 +0100

    deleted exp params in jenkins file

--- script.groovy
+++ script.groovy
@@ -1,3 +1,12 @@
-def function() {
- 
+def buildApp() {
+ 	echo "building App ..."
 }
+def testApp() {
+ 	echo "testing App ..."
+}
+def deployApp() {
+ 	echo "deploying App ..."
+ 	echo "deploying version ${VERSION}"
+}
+
+return this
